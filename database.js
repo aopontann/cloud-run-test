a3df79f485eecf387cc93315ee1ec35b70fa5e46
aopontann <aopontan0416@gmail.com>
2021-04-04 23:00:49 +0900

    Sequelize 導入

--- database.js
+++ database.js
@@ -1,18 +1,23 @@
-const Knex = require('knex');
-const connect = () => {
-  const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    socketPath: process.env.DB_SOCKET,
-  };
+const { Sequelize } = require('sequelize');
 
-  return Knex({
-    client: 'mysql',
-    version: '5.7',
-    connection: config,
-  });
-};
-const knex = connect();
-module.exports = knex;
+const sequelize = new Sequelize("nijisanji_songs_video", "root", "pass1234", {
+  dialect: "mysql",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+(async()=>{
+  const rows = await sequelize.query('select * from Videos');
+  console.log(rows);
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = sequelize;
