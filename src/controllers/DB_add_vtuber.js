cd7f8f6e5e3a8e039061635afc0857729fafec24
aopontann <aopontan0416@gmail.com>
2021-04-12 15:49:35 +0900

    Vtuber 追加機能 実装

---
+++ src/controllers/DB_add_vtuber.js
@@ -0,0 +1,18 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports = async function(body) {
+  for await (const vtuberInfo of body) {
+    await prisma.vtuber.create({
+      data: {
+        id: vtuberInfo.channelId,
+        name: vtuberInfo.name,
+        readname: vtuberInfo.readname,
+        affiliation: vtuberInfo.affiliation,
+        birthday: vtuberInfo.birthday,
+        image: vtuberInfo.image
+      },
+    });
+  }
+  await prisma.$disconnect();
+}
