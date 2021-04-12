f23965ec259c51375522cdc7c61c08bbb11ae023
aopontann <aopontan0416@gmail.com>
2021-04-12 15:50:08 +0900

    Vtuber 追加機能 対応

--- src/routes/DB_vtuber.js
+++ src/routes/DB_vtuber.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-//const DB_add_vtuber = require("../controllers/DB_add_vtuber");
+const DB_add_vtuber = require("../controllers/DB_add_vtuber");
 const DB_get_vtuber = require("../controllers/DB_get_vtuber");
 
 // http://localhost:8080/DB/vtuber?
@@ -10,16 +10,16 @@ const DB_get_vtuber = require("../controllers/DB_get_vtuber");
 router.get("/", async function (req, res) {
   console.log("query", req.query);
   const result = await DB_get_vtuber(req.query);
-  console.log(result);
 
+  console.log(result);
   res.json(result);
 });
 
 router.post("/", async function (req, res) {
-  const result = await DB_add_vtuber(req.body);
+  await DB_add_vtuber(req.body);
 
   res.json({
-    message: result,
+    message: "success!",
     body: req.body,
   });
 });
