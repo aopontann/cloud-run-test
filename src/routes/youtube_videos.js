9ae20cb5505e839e65dd3805d0a3e7bf7cf2e45e
aopontann <aopontan0416@gmail.com>
2021-04-12 20:21:56 +0900

    res 形式変更

--- src/routes/youtube_videos.js
+++ src/routes/youtube_videos.js
@@ -12,19 +12,20 @@ router.get("/", async function (req, res) {
 
   if(req.query.select === "yes") {
     console.log("取得したデータを歌動画か判別するよ");
-    // 判別できたデータは result_selectedVideos.confirm[]に格納する
-    // 判別が難しいデータは result_selectedVideos.unconfirm[]に格納する
+    // 判別できたデータは result_select_Videos.songConfirm = true になる
+    // 判別が難しいデータは result_select_Videos.songConfirm = false になる
     const result_select_videos = select_videos(
       result_getVideoInfo
     );
     res.json(result_select_videos);
 
   } else if(req.query.select === "no") {
-    console.log("取得したデータをそのまま返すよ");
-    res.json({
-      confirm: result_getVideoInfo,
-      unconfirm: []
-    });
+    console.log("取得したデータを歌動画か判別せず返すよ");
+    const result_select_videos = result_getVideoInfo.map(videoInfo => {
+      videoInfo.songConfirm = true;
+      return videoInfo;
+    })
+    res.json(result_select_videos);
     
   } else {
     res.json({
