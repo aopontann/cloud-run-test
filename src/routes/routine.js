e410b1d84d9cf1ef4f0d473a0ed58c1a4c63cd5c
aopontann <aopontan0416@gmail.com>
2021-04-15 14:30:57 +0900

    routine 実装 v1.0.0

--- src/routes/routine.js
+++ src/routes/routine.js
@@ -2,33 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 const DB_get_vtuber = require("../controllers/DB_get_vtuber");
-const DB_get_videos = require("../controllers/DB_get_videos");
-const get_youtube_search_videos = require("../controllers/get_youtube_search");
-const get_youtube_videos = require("../controllers/get_youtube_videos");
+const get_youtube_activites = require("../controllers/youtube/get_youtube_activites");
+const get_youtube_videos = require("../controllers/youtube/get_youtube_videos");
+const select_youtube_videos = require("../controllers/youtube/select_youtube_videos");
+const DB_add_videos = require("../controllers/DB_add_video");
 
 router.get("/", async function (req, res) {
-  /*
   const result_DB_get_vtuber = await DB_get_vtuber({
-    Affiliation: "にじさんじ",
+    Affiliation: "にじさんじ,にじさんじユニット"
   });
-  console.log("result_DB_get_vtuber", result_DB_get_vtuber);
+  //console.log("result_DB_get_vtuber", result_DB_get_vtuber);
   const search_vtuber_channelId = result_DB_get_vtuber.map(
-    (doc) => doc.channelId
+    (vtuber) => vtuber.id
   );
-  */
-
-  /* quota error になった
+  console.log("search_vtuber_channelId", search_vtuber_channelId);
+  
   const datatimeAfter = "2021-01-01T00:00:00Z";
   const datatimeBefore = "2021-01-01T23:59:59Z"
-  const result_get_youtube_search = await get_youtube_search_videos(search_vtuber_channelId, datatimeAfter, datatimeBefore);
-  */
-  const result_DB_get_videos = await DB_get_videos([]);
-  const youtube_videos_id = result_DB_get_videos.map((doc) => doc.id);
-  const result_get_youtube_videos = await get_youtube_videos(youtube_videos_id);
-  console.log(youtube_videos_id);
+  const result_get_youtube_activites = await get_youtube_activites(search_vtuber_channelId, datatimeAfter, datatimeBefore);
+  console.log(result_get_youtube_activites); // videoId, videoId, ...
 
-  res.send(youtube_videos_id);
-  //const result_search_videos = get_search_videos()
+  // 動画の詳細データを取得する
+  const result_get_youtube_videos = await get_youtube_videos(result_get_youtube_activites.split(','));
+  // 歌ってみた動画か判定する
+  const result_select_youtube_videos = select_youtube_videos(result_get_youtube_videos);
+  // 判定した動画データをDBに保存する
+  //await DB_add_videos(result_select_youtube_videos);
+
+  res.json({
+    message: "routine success!",
+    saveVideos: result_select_youtube_videos
+  });
+
+  //res.send({message: "ok!"});
 });
 
 //routerをモジュールとして扱う準備
