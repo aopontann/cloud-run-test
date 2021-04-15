7d24fc6fcb667970ab6727ccccc20dd0162f5c75
aopontann <aopontan0416@gmail.com>
2021-04-15 14:28:42 +0900

    動画削除機能 追加

--- src/routes/DB_videos.js
+++ src/routes/DB_videos.js
@@ -1,29 +1,56 @@
 const express = require("express");
 const router = express.Router();
 
-const DB_add_videos = require("../controllers/DB_add_videos");
-const DB_get_videos = require("../controllers/DB_get_videos");
+const DB_add_videos = require("../controllers/DB_add_video");
+const DB_get_videos = require("../controllers/DB_get_video");
+const DB_delete_videos = require("../controllers/DB_delete_video");
+const get_joinVtuber = require("../controllers/get_joinVtuber");
 
-// http://localhost:3002/DB/videos?
-// id="videoId,videoId, ..."
+// http://localhost:3002/DB/videos?id="videoId,videoId, ..." or id="all"
 router.get("/", async function (req, res) {
-  const id = req.query.id;
+  const id = req.query.id || null;
   console.log("id", id);
-  const result = await DB_get_videos(id.split(","));
+  const result = id ? await DB_get_videos(id.split(",")) : null;
   console.log(result);
 
   res.json(result);
 });
 
-//http://localhost:3002/DB/videos?confirm= (yes or no)
+//http://localhost:3002/DB/videos
 router.post("/", async function (req, res) {
   console.log(req.body);
-  await DB_add_videos(req.body, req.query.confirm == "yes" ? true : false);
+  await DB_add_videos(req.body);
 
   res.json({
     message: "success!",
   });
 });
+
+// http://localhost:8080/DB/videos?id="videoId"
+router.delete("/", async function (req, res) {
+  const id = req.query.id || null;
+  if (!id || id == "") {
+    console.log("error!");
+    res.json({
+      message: "error!",
+    });
+    return;
+  }
+  console.log("消す動画ID", id);
+  try {
+    await DB_delete_videos(id);
+  } catch (e) {
+    res.json({
+      message: "指定されたIDが見つかりません",
+    });
+    return;
+  }
+
+  res.json({
+    message: "success!",
+  });
+});
+
 
 //routerをモジュールとして扱う準備
 module.exports = router;
