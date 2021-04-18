95ca8f3107335e356c2f0c477c64781ba16a2122
aopontann <aopontan0416@gmail.com>
2021-04-18 17:17:24 +0900

    正規表現 入力形式 変更

--- src/controllers/get_search_songVtuber.js
+++ src/controllers/get_search_songVtuber.js
@@ -1,35 +1,35 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-module.exports = async function(all_videoInfo) {
+module.exports = async function (videoInfo) {
   const all_search_vtuber = await prisma.vtuber.findMany({
     where: {
       NOT: {
-        name: "にじさんじ"
-      }
-    }
+        name: "にじさんじ",
+      },
+    },
   });
   //const all_search_vtuber = result_findMany.map(vtuber => vtuber.name);
   let result_search_vtuber = [];
 
-  all_videoInfo.forEach(videoInfo => {
-    all_search_vtuber.forEach(vtuber => {
-      if (videoInfo.snippet.description.match(vtuber.name) || videoInfo.snippet.title.match(vtuber.name)) {
-        result_search_vtuber.push({
-          videoId: videoInfo.id,
-          channelId: vtuber.id,
-          role: "歌"
-        });
-      }
-    });
+  all_search_vtuber.forEach((vtuber) => {
+    const reg = new RegExp(vtuber.name)
+    if (
+      videoInfo.snippet.description.match(reg) ||
+      videoInfo.snippet.title.match(reg)
+    ) {
+      result_search_vtuber.push({
+        videoId: videoInfo.id,
+        channelId: vtuber.id,
+        role: "歌",
+      });
+    }
   });
 
   return result_search_vtuber;
-}
+};
 
-
-/* all_videoInfo (sample)
-[
+/* videoInfo (sample)
     {
         "kind": "youtube#video",
         "etag": "MnxfMb6LV5Tnn4akp44EvIVhYDM",
@@ -96,73 +96,5 @@ module.exports = async function(all_videoInfo) {
             "scheduledStartTime": "2020-03-01T08:30:00Z"
         },
         "songConfirm": true
-    },
-    {
-        "kind": "youtube#video",
-        "etag": "f3ArOD8W_D6ChdqDWAWvZhBCDZM",
-        "id": "pQxDWYMqjRU",
-        "snippet": {
-            "publishedAt": "2020-03-04T14:52:38Z",
-            "channelId": "UCuep1JCrMvSxOGgGhBfJuYw",
-            "title": "【スマブラSP】おれはつよい・・・おれはつよい・・【フレン・Ｅ・ルスタリオ/にじさんじ】",
-            "description": "配信タグ：#ルスタリオンエア\n\nみ～～んなまとめてたおしたるかんな＾＾\n\n\nにじさんじ所属新人バーチャルライバーの\nフレン・E・ルスタリオです\n\n応援してね\n\nチャンネル登録、高評価、通知設定よかったらよろしく！\n\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー\n\n◆ Twitter\nhttps://twitter.com/furen_2434\n◆チャンネル登録\nhttps://www.youtube.com/channel/UCuep...\n\n◆生放送関連のツイート #ルスタリオンエア\n◆ファントーク #フレン親衛隊\n◆ファンアート #フレン見て\n\n\n【にじさんじ関連】\n\n◆ 公式HP\nhttps://nijisanji.ichikara.co.jp/\n\n◆ 公式Twitter\nhttps://twitter.com/Nijisanji_app\n\n◆ Booth公式Twitter\nhttps://twitter.com/booth_pm\n\n◆ 公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\n◆ お問い合わせやプレゼントはこちら\nhttps://nijisanji.ichikara.co.jp/cont...\n\nファンレター・プレゼントは、下記住所宛てにお送り下さい。\n\n〒175-0082\n東京都板橋区高島平6-2-1\nネットデポ新高島平内\nいちから株式会社 フレン・E・ルスタリオ宛\n\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー",
-            "thumbnails": {
-                "default": {
-                    "url": "https://i.ytimg.com/vi/pQxDWYMqjRU/default.jpg",
-                    "width": 120,
-                    "height": 90
-                },
-                "medium": {
-                    "url": "https://i.ytimg.com/vi/pQxDWYMqjRU/mqdefault.jpg",
-                    "width": 320,
-                    "height": 180
-                },
-                "high": {
-                    "url": "https://i.ytimg.com/vi/pQxDWYMqjRU/hqdefault.jpg",
-                    "width": 480,
-                    "height": 360
-                },
-                "standard": {
-                    "url": "https://i.ytimg.com/vi/pQxDWYMqjRU/sddefault.jpg",
-                    "width": 640,
-                    "height": 480
-                },
-                "maxres": {
-                    "url": "https://i.ytimg.com/vi/pQxDWYMqjRU/maxresdefault.jpg",
-                    "width": 1280,
-                    "height": 720
-                }
-            },
-            "channelTitle": "フレン・E・ルスタリオ",
-            "categoryId": "20",
-            "liveBroadcastContent": "none",
-            "localized": {
-                "title": "【スマブラSP】おれはつよい・・・おれはつよい・・【フレン・Ｅ・ルスタリオ/にじさんじ】",
-                "description": "配信タグ：#ルスタリオンエア\n\nみ～～んなまとめてたおしたるかんな＾＾\n\n\nにじさんじ所属新人バーチャルライバーの\nフレン・E・ルスタリオです\n\n応援してね\n\nチャンネル登録、高評価、通知設定よかったらよろしく！\n\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー\n\n◆ Twitter\nhttps://twitter.com/furen_2434\n◆チャンネル登録\nhttps://www.youtube.com/channel/UCuep...\n\n◆生放送関連のツイート #ルスタリオンエア\n◆ファントーク #フレン親衛隊\n◆ファンアート #フレン見て\n\n\n【にじさんじ関連】\n\n◆ 公式HP\nhttps://nijisanji.ichikara.co.jp/\n\n◆ 公式Twitter\nhttps://twitter.com/Nijisanji_app\n\n◆ Booth公式Twitter\nhttps://twitter.com/booth_pm\n\n◆ 公式オンラインショップ\nhttps://nijisanji.booth.pm/\n\n◆ お問い合わせやプレゼントはこちら\nhttps://nijisanji.ichikara.co.jp/cont...\n\nファンレター・プレゼントは、下記住所宛てにお送り下さい。\n\n〒175-0082\n東京都板橋区高島平6-2-1\nネットデポ新高島平内\nいちから株式会社 フレン・E・ルスタリオ宛\n\nーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー"
-            }
-        },
-        "contentDetails": {
-            "duration": "PT1H2M6S",
-            "dimension": "2d",
-            "definition": "hd",
-            "caption": "false",
-            "licensedContent": true,
-            "contentRating": {},
-            "projection": "rectangular"
-        },
-        "statistics": {
-            "viewCount": "35410",
-            "likeCount": "1388",
-            "dislikeCount": "46",
-            "favoriteCount": "0",
-            "commentCount": "37"
-        },
-        "liveStreamingDetails": {
-            "actualStartTime": "2020-03-04T13:41:10Z",
-            "actualEndTime": "2020-03-04T14:43:10Z",
-            "scheduledStartTime": "2020-03-04T13:45:00Z"
-        },
-        "songConfirm": true
     }
-]
 */
