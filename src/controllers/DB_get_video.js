74204484853ce10c7da93e8a337e7c8d90f4dc0e
aopontann <aopontan0416@gmail.com>
2021-04-19 23:34:20 +0900

    update

--- src/controllers/DB_get_video.js
+++ src/controllers/DB_get_video.js
@@ -1,15 +1,55 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-module.exports = async function(all_videoId) {
-  if (all_videoId[0] == "all") {
+/* query {
+  id: "videoId, videoId, ..." or "" or "all"
+  songConfirm: "true" or "false" or "all"
+  checkSongVtuber: "true" or "false" or "all"
+}
+true, false, all 以外は false として扱う
+*/ 
+
+module.exports = async function (query) {
+  console.log("query", query);
+  if (!query.id) {
+    return [];
+  }
+  const all_videoId = query.id.split(",");
+  const songConfirm = query.songConfirm;
+  const checkSongVtuber =query.checkSongVtuber;
+
+  const include = {
+    thumbnail: true,
+    time: true,
+    dayCount: true,
+    songVtuber: {
+      select: {
+        role: true,
+        vtuber: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    },
+  };
+  const whereAND = {};
+  if (songConfirm != "all") {
+    whereAND.songConfirm = songConfirm == "true" ? true : false;
+  }
+  if (checkSongVtuber != "all") {
+    whereAND.checkSongVtuber = checkSongVtuber == "true" ? true : false;
+  }
+  if (query.id != "all") {
+    whereAND.id = { in: all_videoId };
+  }
+  console.log("whereAND", whereAND);
+
+  // 全ての動画を取得する
+  if (query.id == "all" && songConfirm == "all" && checkSongVtuber == "all") {
     const getVideo = await prisma.videos.findMany({
-      include: {
-        statistic: true,
-        thumbnail: true,
-        time: true,
-        SongVtuber: true
-      }
+      include: include
     });
     await prisma.$disconnect();
     return getVideo;
@@ -17,15 +57,10 @@ module.exports = async function(all_videoId) {
 
   const getVideo = await prisma.videos.findMany({
     where: {
-      id: { in: all_videoId }
+      AND: whereAND,
     },
-    include: {
-      statistic: true,
-      thumbnail: true,
-      time: true,
-      SongVtuber: true
-    }
+    include: include,
   });
   await prisma.$disconnect();
   return getVideo;
-}
+};
