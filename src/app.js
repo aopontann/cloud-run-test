29783391f028ede068ecc6aac4acd1608e5bf4a1
aopontann <aopontan0416@gmail.com>
2021-04-28 16:48:10 +0900

    rename

--- src/app.js
+++ src/app.js
@@ -8,14 +8,9 @@ app.use(express.json());
 
 const port = process.env.PORT || 8080; // port番号を指定
 
-const youtube_activities_router = require('./routes/youtube_activities');
-app.use('/youtube/activities', youtube_activities_router);
 
-const youtube_search_router = require('./routes/youtube_search');
-app.use('/youtube/search', youtube_search_router);
-
-const youtube_videos_router = require('./routes/youtube_videos');
-app.use('/youtube/videos', youtube_videos_router);
+const youtube_router = require('./routes/youtube');
+app.use('/youtube', youtube_router);
 
 const DB_videos_router = require('./routes/DB_videos');
 app.use('/DB/videos', DB_videos_router);
@@ -26,7 +21,6 @@ app.use('/DB/vtuber', DB_vtuber_router);
 const routin_router = require('./routes/routine');
 app.use('/routine', routin_router);
 
-
 //サーバ起動
 app.listen(port);
-console.log('listen on port ' + port);
+console.log('niji-song-server port ' + port);
