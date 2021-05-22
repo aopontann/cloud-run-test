9f5d717aa81d43bb5d3f4de929eda7e1f0947f07
aopontann <aopontan0416@gmail.com>
2021-05-22 14:50:25 +0900

    update

--- src/controllers/video/DB_update_video.js
+++ src/controllers/video/DB_update_video.js
@@ -2,14 +2,17 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 module.exports = async function (body) {
-  if (!body || !body.id) {
-    console.log("body error");
+  if (!body || !body.videoId) {
+    console.log("videoUpdate body error");
     return "body error";
   }
+  const videoId = body.videoId;
+  const songConfirm = body.songConfirm || false;
+  const checkSongVtuber = body.checkSongVtuber || false;
   // 指定した動画IDがDBに存在するか調べる
   const search_videoId = await prisma.videos.findFirst({
     where: {
-      id: body.id
+      id: videoId
     }
   });
   if (!search_videoId) {
@@ -17,23 +20,38 @@ module.exports = async function (body) {
     return "not exist";
   }
   // 一旦、動画に出演しているVtuber情報を削除する
+  /*
   const deleteSongVtuber = prisma.songVtuber.deleteMany({
     where: {
       videoId: body.id,
     },
   });
+  */
   // 歌ってみた動画か自分で確認してDBに保存する
   // 出演Vtuberを確認したら自分で確認済みチェックを入れることができる checkSongVtuber
-  const updateSongConfirm = prisma.videos.update({
+  const updateSongConfirm = await prisma.videos.update({
     where: {
-      id: body.id,
+      id: videoId,
     },
     data: {
-      songConfirm: body.songConfirm || search_videoId.songConfirm,
-      checkSongVtuber: body.checkSongVtuber || search_videoId.checkSongVtuber
+      songConfirm: songConfirm,
+      checkSongVtuber: checkSongVtuber
     },
+  }).catch((e) => {
+    console.log("update video error!");
+    return "update video error!";
   });
+  /*
+  const findResult = await prisma.videos.findFirst({
+    where: {
+      id: videoId
+    }
+  });
+  */
+  await prisma.$disconnect();
+  return updateSongConfirm;
   // 動画に出演しているVtuber情報を登録する
+  /*
   const updateSongVtuber = prisma.songVtuber.createMany({
     data: body.songVtuber.map((array) => {
       return {
@@ -43,54 +61,16 @@ module.exports = async function (body) {
       };
     }),
   });
-
-  await prisma.$transaction([
-    deleteSongVtuber,
-    updateSongConfirm,
-    updateSongVtuber
-  ]);
-  
-  await prisma.$disconnect();
-  return "update success";
+  */
 };
 
 /* body
 {
     "videoId": "1CaG8DhNcy0",
-    "songConfirm": true,
-    "checkSongVtuber" : true,
-    "updateSongVtuber": [
+    "joinVtuber": [
             {
-                "confirm": false,
-                "role": "歌",
-                "vtuber": {
-                    "id": "UC_GCs6GARLxEHxy1w40d6VQ",
-                    "name": "家長むぎ"
-                }
-            },
-            {
-                "confirm": false,
-                "role": "歌",
-                "vtuber": {
-                    "id": "UC48jH1ul-6HOrcSSfoR02fQ",
-                    "name": "夕陽リリ"
-                }
-            },
-            {
-                "confirm": false,
-                "role": "歌",
-                "vtuber": {
-                    "id": "UCBiqkFJljoxAj10SoP2w2Cg",
-                    "name": "文野環"
-                }
-            },
-            {
-                "confirm": false,
-                "role": "歌",
-                "vtuber": {
-                    "id": "UCtpB6Bvhs1Um93ziEDACQ8g",
-                    "name": "森中花咲"
-                }
+                "channelId": "UCsg-YqdqQ-KFF0LNk23BY4A",
+                "role": "歌"
             }
         ]
 }
