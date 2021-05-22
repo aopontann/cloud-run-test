38a73e9fd91a2674ddfd4886d548df41bf6b1874
aopontann <aopontan0416@gmail.com>
2021-05-22 14:45:58 +0900

    add updateVtuber & addVtuberImage

--- src/routes/vtuber.js
+++ src/routes/vtuber.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const DB_add_vtuber = require("../controllers/vtuber/DB_add_vtuber");
 const DB_get_vtuber = require("../controllers/vtuber/DB_get_vtuber");
-const test = require("../controllers/youtube/select_youtube_videos");
 const DB_delete_songVtuber = require("../controllers/songVtuber/delete_songVtuber");
+const update_vtuber = require("../controllers/vtuber/update_vtuber");
+const add_vtuberImage = require("../controllers/vtuber/add_vtuberImage");
 
 // http://localhost:8080/DB/vtuber?
 // affiliation= (にじさんじ, ...) & name= (える, ...)
@@ -15,6 +16,7 @@ router.get("/", async function (req, res) {
   const names = req.query.name ? req.query.name.split(",") : null;
   const channelId = req.query.channelId ? req.query.channelId.split(",") : null;
   const include = req.query.include ? req.query.include.split(",") : []; // ["songVtuber", "vtuberImage"]
+
   const result = await DB_get_vtuber({
     affiliation: affi,
     name: names,
@@ -27,27 +29,47 @@ router.get("/", async function (req, res) {
 });
 
 router.post("/", async function (req, res) {
-  await DB_add_vtuber(req.body);
-
-  res.json({
-    message: "success!",
-    body: req.body,
-  });
+  let errorFlag = false;
+  await DB_add_vtuber([...req.body])
+    .catch((e) => {
+      console.log("error!");
+      errorFlag = true;
+    })
+    .finally(() => {
+      errorFlag ? res.json("error") : res.json("success");
+    });
 });
 
-router.delete("/songVtuber", async function (req, res) {
-  // channelId = "id, id, ...", delete_videoId = "videoId"
-  // or channelId = "id", delete_videoId = "videoId, videoId, ..."
-  const channelId = req.query.channelId;
-  const delete_videoId = req.query.videoId;
-  await DB_delete_songVtuber({
-    channelId: channelId,
-    deleteId: delete_videoId,
-  });
-  res.json({
-    message: "success",
-  });
+router.put("/", async function (req, res) {
+  let errorFlag = false;
+  await update_vtuber({
+    channelId: req.query.channelId || null,
+    name: req.query.name || null,
+    readname: req.query.readname || null,
+    affiliation: req.query.affiliation || null,
+    birthday: req.query.birthday || null,
+  })
+    .catch((e) => {
+      console.log("update_vtuber error!");
+      errorFlag = true;
+    })
+    .finally(() => {
+      errorFlag ? res.json("error!") : res.json("success!");
+    });
 });
+
+router.post("/image", async function (req, res) {
+  let errorFlag = false;
+  await add_vtuberImage([...req.body])
+    .catch((e) => {
+      errorFlag = true;
+      console.log("create vtuberImage error!");
+    })
+    .finally(() => {
+      errorFlag ? res.json("error") : res.json("success");
+    });
+});
+
 
 //routerをモジュールとして扱う準備
 module.exports = router;
