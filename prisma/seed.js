148e32a8628bd6f5293f0dd23b53cd044ed79665
aopontann <aopontan0416@gmail.com>
2021-05-22 14:40:45 +0900

    create → upsert

--- prisma/seed.js
+++ prisma/seed.js
@@ -16,19 +16,24 @@ async function main() {
   // vtuber init
   for await (const vtuberInfo of all_vtuberInfo.list) {
     console.log("name", vtuberInfo.name);
-    await prisma.vtuber.create({
-      data: {
-        id: vtuberInfo.id,
-        name: vtuberInfo.name,
-        readname: vtuberInfo.readname,
-        affiliation: vtuberInfo.affiliation,
-        birthday: vtuberInfo.birthday
-      },
-    }).catch((e) => {
-      console.log("error!!!!!!");
-    }).finally(() => {
-      console.log("success!");
-    });
+    await prisma.vtuber
+      .upsert({
+        where: { id: vtuberInfo.id },
+        create: {
+          id: vtuberInfo.id,
+          name: vtuberInfo.name,
+          readname: vtuberInfo.readname,
+          affiliation: vtuberInfo.affiliation,
+          birthday: vtuberInfo.birthday,
+        },
+        update: {}
+      })
+      .catch((e) => {
+        console.log("error!!!!!!");
+      })
+      .finally(() => {
+        console.log("success!");
+      });
   }
   // video test data
   await prisma.videos.create({
@@ -37,15 +42,15 @@ async function main() {
       title: "【】歌ってみた KING 葛葉 【】",
       thumbnail: {
         create: {
-          defaultUrl: "https://i.ytimg.com/vi/oPAcjv__fbc/default.jpg"
-        }
+          defaultUrl: "https://i.ytimg.com/vi/oPAcjv__fbc/default.jpg",
+        },
       },
       time: {
         create: {
           videoLength: "PT2M15S",
           startTime: "2020-11-10T11:00:00Z",
-          createdAt: formatted_now
-        }
+          createdAt: formatted_now + "Z",
+        },
       },
       dayCount: {
         create: {
@@ -53,17 +58,19 @@ async function main() {
           likeCount: 304201,
           dislikeCount: 2363,
           commentCount: 21016,
-          createdAt: formatted_now
-        }
+          createdAt: formatted_now + "Z",
+        },
       },
       songVtuber: {
         create: {
           role: "歌",
           channelId: "UCSFCh5NL4qXrAy9u-u2lX3g",
-          createdAt: formatted_now
-        }
-      }
-    }
+          createdAt: formatted_now + "Z",
+        },
+      },
+    },
+  }).catch((e) => {
+    console.log("seed error!")
   });
 }
 
