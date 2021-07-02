311896ef1ccc2596cdeeb9dae8b1ae4d3b6cd45a
aopontann <aopontan0416@gmail.com>
2021-07-02 22:53:10 +0900

    update: get_times get_time2にdiff機能追加

--- src/controllers/get_times.ts
+++ src/controllers/get_times.ts
@@ -6,6 +6,7 @@ const FORMAT = "YYYY-MM-DDTHH:mm:ss";
 interface Query {
   format?: string;
   timezone?: string;
+  hour_diff?: number;
 }
 
 export function get_time(TIME_ZONE: string, time_hour_diff: number): string {
@@ -17,9 +18,10 @@ export function get_time(TIME_ZONE: string, time_hour_diff: number): string {
   return formatted_time + "Z";
 }
 
-export function get_time2(q: Query | null): string {
+export function get_time2(q?: Query | null): string {
   const query: Query = q || {};
   const time: Date = new Date();
+  q?.hour_diff ? time.setHours(time.getHours() + q.hour_diff) : "";
   const format: string = query.format || "YYYY-MM-DDTHH:mm:ss";
   const timezone: string = query.timezone || "Asia/Tokyo";
   const formatted_time: string = formatToTimeZone(time, format, {
@@ -28,9 +30,8 @@ export function get_time2(q: Query | null): string {
   return formatted_time + "Z";
 }
 
-/*
 // JST 2021-05-23T16:22:24Z
-module.exports.toUTC = (JST) => {
+export function toUTC(JST: string): string {
   const time = Date.parse(JST);
   const date = new Date(time);
   date.setHours(date.getHours() - 9);
@@ -38,15 +39,14 @@ module.exports.toUTC = (JST) => {
     timeZone: "UTC",
   });
   return formatted_time + "Z";
-};
+}
 
-module.exports.toJST = (UTC) => {
+export function toJST(UTC: string): string {
   const time = Date.parse(UTC);
   const date = new Date(time);
   //date.setHours(date.getHours() + 9);
   const formatted_time = formatToTimeZone(date, FORMAT, {
-    timeZone: TIME_ZONE_TOKYO,
+    timeZone: "Asia/Tokyo",
   });
   return formatted_time + "Z";
-};
-*/
+}
