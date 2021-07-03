5e1b6913491ee2e42e5748c1b1a3d397e40dbf01
aopontann <aopontan0416@gmail.com>
2021-07-03 22:26:11 +0900

    update: update_statistics

--- src/controllers/update_statistics.ts
+++ src/controllers/update_statistics.ts
@@ -2,36 +2,20 @@ import { youtube_v3 } from "googleapis";
 import prisma from "../../prisma/client";
 import { get_time2 } from "./get_times";
 
-interface Query {
-  kind: string;
-  etag: string;
-  id: string;
-  statistics: youtube_v3.Schema$VideoStatistics
-}
-
-export default async function (query: Query[]): Promise<void> {
+export default async function (query: youtube_v3.Schema$Video[]): Promise<void> {
   console.log("update_viewCount");
   let cnt = 1;
   for await (const video of query) {
     const count = video.statistics;
     console.log(`(${cnt++}/${query.length}) id = ${video.id}`)
-    await prisma.statistics.upsert({
-      where: { id: video.id },
-      update: {
+    await prisma.statistics.update({
+      where: { id: video.id || "error" },
+      data: {
         updatedAt: get_time2({}),
-        viewCount: count.viewCount ? Number(count.viewCount) : null,
-        likeCount: count.likeCount ? Number(count.likeCount) : null,
-        dislikeCount: count.dislikeCount ? Number(count.dislikeCount) : null,
-        commentCount: count.commentCount ? Number(count.commentCount) : null,
-      },
-      create: {
-        id: video.id,
-        createdAt: get_time2({}),
-        updatedAt: get_time2({}),
-        viewCount: count.viewCount ? Number(count.viewCount) : null,
-        likeCount: count.likeCount ? Number(count.likeCount) : null,
-        dislikeCount: count.dislikeCount ? Number(count.dislikeCount) : null,
-        commentCount: count.commentCount ? Number(count.commentCount) : null,
+        viewCount: count?.viewCount ? Number(count.viewCount) : undefined,
+        likeCount: count?.likeCount ? Number(count.likeCount) : undefined,
+        dislikeCount: count?.dislikeCount ? Number(count.dislikeCount) : undefined,
+        commentCount: count?.commentCount ? Number(count.commentCount) : undefined,
       },
     }).catch((e) => {
       console.error("update_statistics error!");
