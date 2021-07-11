d5bbb631bb554f13f75091051f1f7d26d303f927
aopontann <aopontan0416@gmail.com>
2021-07-11 11:36:47 +0900

    update: get "/" get "/video"

--- src/routes/tags.ts
+++ src/routes/tags.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { youtube_v3 } from "googleapis";
 import search_vtuberName from "../controllers/tag/search_vtuberName";
 import get_tags_relation_video from "../controllers/tag/get_tags_relation_video";
+import get_tags from "../controllers/tag/get_tags";
 import add_tag from "../controllers/tag/add_tag";
 import delete_tag from "../controllers/tag/delete_tag";
 
@@ -15,13 +16,38 @@ interface TagBody {
   }[];
 }
 
-router.get("/", async(req: express.Request, res: express.Response) => {
-  const names = req.query.names ? (req.query.names as string).split(",") : undefined;
+router.get("/", async (req: express.Request, res: express.Response) => {
+  const names = req.query.names
+    ? (req.query.names as string).split(",")
+    : undefined;
+
+  const Result = await get_tags({
+    names,
+  }).catch((e) => {
+    console.error("get_tags error");
+    res.status(500).json({
+      error: "get_tags error",
+    });
+    throw e;
+  });
+
+  res.status(500).json(Result);
+});
+
+router.get("/videos", async (req: express.Request, res: express.Response) => {
+  const names = req.query.names
+    ? (req.query.names as string).split(",")
+    : undefined;
   const songConfirm = req.query?.songConfirm || undefined;
-  const startAtAfter = req.query?.startAtAfter as string || undefined;
-  const startAtBefore = req.query?.startAtBefore as string || undefined;
-  const maxResults = req.query?.maxResults ? Number(req.query?.maxResults) : undefined;
-  const page = req.query?.page && Number(req.query.page) > 0 ? Number(req.query.page) : undefined;
+  const startAtAfter = (req.query?.startAtAfter as string) || undefined;
+  const startAtBefore = (req.query?.startAtBefore as string) || undefined;
+  const maxResults = req.query?.maxResults
+    ? Number(req.query?.maxResults)
+    : undefined;
+  const page =
+    req.query?.page && Number(req.query.page) > 0
+      ? Number(req.query.page)
+      : undefined;
 
   const Result = await get_tags_relation_video({
     names,
@@ -32,17 +58,17 @@ router.get("/", async(req: express.Request, res: express.Response) => {
     startAtAfter,
     startAtBefore,
     maxResults,
-    page
-  }).catch(e => {
+    page,
+  }).catch((e) => {
     console.error("get_tag_video error");
-        res.status(500).json({
-          error: "get_tag_video error",
-        });
-        throw e;
+    res.status(500).json({
+      error: "get_tag_video error",
+    });
+    throw e;
   });
 
   res.status(500).json(Result);
-})
+});
 
 router.post("/", async (req: express.Request, res: express.Response) => {
   const youtube_video = req.body.youtube_video as
@@ -77,15 +103,10 @@ router.post("/", async (req: express.Request, res: express.Response) => {
 });
 
 router.delete("/", async (req: express.Request, res: express.Response) => {
-  const tagName: string[] | null = req.query.name
+  const tagName = req.query.name
     ? (req.query.name as string).split(",")
-    : null; //errorになるかも
+    : undefined; //errorになるかも
   const videoId = req.query.videoId as string | undefined;
-
-  if (!tagName) {
-    res.status(500).json({ error: "nameを指定してください" });
-    return;
-  }
 
   await delete_tag({ names: tagName, videoId: videoId }).catch((e) => {
     console.error("delete_tags error");
