d373f7e05bf394d0018077699841621bf49ac4bb
aopontann <aopontan0416@gmail.com>
2021-08-13 23:11:48 +0900

    update: add_tags コードの量を減らしわかりやすくした

--- src/controllers/tag/add_tag.ts
+++ src/controllers/tag/add_tag.ts
@@ -1,59 +1,30 @@
 import prisma from "../../client";
 import delete_tag from "./delete_tag";
 
-export default async function (
-  query: {
-    videoId: string;
-    tags: {
-      name: string;
-      description?: string;
-    }[];
-  }[]
-): Promise<void> {
-  /*
-  const query_test = [{
-    videoId: "097qC2EjLHg",
-    tags: [
-      {
-        name: "alex1",
-        description: "abc",
-      },
-      {
-        name: "alex2",
-        description: "edf", 
-      },
-    ]
-  }];
-  */
+export default async function (query: {
+  videoId: string;
+  tags: {
+    name: string;
+    type?: string;
+  }[];
+}): Promise<void> {
 
-  for await (const videoTag of query) {
-    console.log("add_tag videoId=", videoTag.videoId);
+  console.log("add_tag videoId=", query.videoId);
+  // 既に保存されているタグを取得
+  const res = await prisma.tags.findMany({
+    where: { videoId: query.videoId },
+  });
+  const already_names = res.map((tag) => tag.name);
 
-    // タグの重複を防ぐための処理 あまりよくないから修正する必要がある
-    await delete_tag({videoId: videoTag.videoId});
+  //
+  const save_names: { name: string; videoId: string; type?: string }[] = [];
+  query.tags.forEach((tag) =>
+    already_names.includes(tag.name)
+      ? ""
+      : save_names.push({ ...tag, videoId: query.videoId })
+  );
 
-    // １つの動画IDと複数のタグ↓
-    for await (const tag of videoTag.tags) {
-      await prisma.tag.upsert({
-        where: { name: tag.name },
-        create: {
-          name: tag.name,
-          tagVideo: {
-            create: {
-              videoId: videoTag.videoId,
-              description: tag.description || undefined,
-            },
-          },
-        },
-        update: {
-          tagVideo: {
-            create: {
-              videoId: videoTag.videoId,
-              description: tag.description || undefined,
-            },
-          },
-        },
-      });
-    }
-  }
+  await prisma.tags.createMany({
+    data: [...save_names],
+  });
 }
