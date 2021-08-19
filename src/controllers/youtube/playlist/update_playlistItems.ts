e864604c78adb8f5bd52daca92e3e9302cc50866
aopontann <aopontan0416@gmail.com>
2021-08-19 23:40:44 +0900

    update: update_playlistItems videoIdとtitleを指定するように変更した

--- src/controllers/youtube/playlist/update_playlistItems.ts
+++ src/controllers/youtube/playlist/update_playlistItems.ts
@@ -8,12 +8,11 @@ import delete_playlistItems from "./delete_playlistItems";
 //指定した期間に公開された全ての動画URLを取得する
 export default async function (q: {
   playlistId: string;
-  startTimeAfter: string;
-  startTimeBefore: string;
+  videoId: string[];
+  title?: string;
 }): Promise<void> {
   const playlistId = q.playlistId;
-  const startAtAfter = q.startTimeAfter;
-  const startAtBefore = q.startTimeBefore;
+  const update_videoId = q.videoId;
   const CREDENTIALS_PATH = "./client_secret.json";
   const TOKEN_PATH = "./token.json";
   const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, "utf8"));
@@ -28,65 +27,53 @@ export default async function (q: {
   oAuth2Client.setCredentials(token);
   const service = google.youtube("v3");
 
-  // 過去1週間分の動画データを取得（動画データ）
-  const result_get_video = await get_video({
-    songConfirm: true,
-    startAtAfter,
-    startAtBefore,
-    order: "startTime"
-  });
-  const res_get_videoId = result_get_video.map((video) => video.id);
-
   // プレイリストに保存されている動画データを取得 (プレイリスト動画データ)
-  const res = await service.playlistItems.list({
-    part: ["id", "snippet"],
-    playlistId,
-    maxResults: 50,
-    auth: oAuth2Client,
-  }).catch(e => {
-    throw e;
-  });
-  const res_playlistItems_videoId = res.data?.items?.map(item => item.snippet?.resourceId?.videoId as string) || [];
-  
+  const res = await service.playlistItems
+    .list({
+      part: ["id", "snippet"],
+      playlistId,
+      maxResults: 50,
+      auth: oAuth2Client,
+    })
+    .catch((e) => {
+      throw e;
+    });
+  const res_playlistItems_videoId =
+    res.data?.items?.map(
+      (item) => item.snippet?.resourceId?.videoId as string
+    ) || [];
+
   // 動画データに含まれていないプレイリスト動画データidを取得
-  const delete_videoId = res_playlistItems_videoId.filter(id => !res_get_videoId.includes(id));
+  const delete_videoId = res_playlistItems_videoId.filter(
+    (id) => !update_videoId.includes(id)
+  );
 
   // プレイリスト動画データに含まれていない動画データidを取得
-  const insert_videoId = res_get_videoId.filter(id => !res_playlistItems_videoId.includes(id));
-  
+  const insert_videoId = update_videoId.filter(
+    (id) => !res_playlistItems_videoId.includes(id)
+  );
+
   // 動画データに含まれていないプレイリスト動画データを削除
   await delete_playlistItems({
     playlistId,
-    videoId: delete_videoId
+    videoId: delete_videoId,
   });
 
   await insert_playlistItems({
     playlistId,
-    videoId: insert_videoId
+    videoId: insert_videoId,
   });
 
-  // .slice(0, -1) 最後の「z」を削除
-  const today = get_time2({
-    format: "MM/DD",
-    time: startAtBefore.slice(0, -1),
-    addZ: false
-  });
-
-  const sixDayAgo = get_time2({
-    format: "MM/DD",
-    time: startAtAfter.slice(0, -1),
-    addZ: false
-  });
-
-  await service.playlists.update({
-    part: ["snippet"],
-    auth: oAuth2Client,
-    requestBody: {
-      "id": "PL_bYerfwKlGiQSckNm6G6D4e-UugXiZrG",
-      "snippet": {
-        "title": `にじさんじ 歌動画リスト (${sixDayAgo} 〜 ${today})`
-      }
-    }
-  });
-
+  q.title
+    ? await service.playlists.update({
+        part: ["snippet"],
+        auth: oAuth2Client,
+        requestBody: {
+          id: playlistId,
+          snippet: {
+            title: q.title,
+          },
+        },
+      })
+    : "";
 }
