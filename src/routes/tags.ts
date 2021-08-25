a5acb032e411b8b2ee12aec7081e96ad9ab56772
aopontann <aopontan0416@gmail.com>
2021-08-25 21:44:12 +0900

    create: add-tag youtube-body

--- src/routes/tags.ts
+++ src/routes/tags.ts
@@ -7,14 +7,6 @@ import delete_tag from "../controllers/tag/delete_tag";
 
 const router = express.Router();
 
-interface TagBody {
-  videoId: string;
-  tags: {
-    name: string;
-    type: string;
-  }[];
-}
-
 router.get("/", async (req: express.Request, res: express.Response) => {
   const names = req.query.names
     ? (req.query.names as string).split(",")
@@ -36,10 +28,7 @@ router.get("/", async (req: express.Request, res: express.Response) => {
 router.post("/", async (req: express.Request, res: express.Response) => {
   const video_tags = req.body as {
     videoId: string;
-    tags: {
-      name: string;
-      type: string | undefined;
-    }[];
+    tagNames: string[];
   } | undefined;
 
   video_tags
@@ -50,31 +39,28 @@ router.post("/", async (req: express.Request, res: express.Response) => {
         });
         throw e;
       })
-    : "";
+    : res.status(400).json("not exist body");
  
   res.status(201).json("success");
+});
 
-  /*
-  const youtube_video = req.body.youtube_video as
-    | youtube_v3.Schema$Video[]
-    | undefined;
-  const result_search = youtube_video
-    ? await search_vtuberName(youtube_video)
-    : null;
-  */
-  // console.log("video_tags", video_tags);
+router.post("/youtube", async (req: express.Request, res: express.Response) => {
+  const youtube_video = req.body as youtube_v3.Schema$Video[]
+  if (typeof youtube_video == "undefined") {
+    res.status(400).json("not exist body");
+  }
+  const result_search = await search_vtuberName(youtube_video).catch(e => {
+    res.status(500).json({error: "add_tag error"});
+    throw e;
+  });
+  for await (const result of result_search) {
+    await add_tag(result).catch(e => {
+      res.status(500).json({error: "add_tag error"});
+      throw e;
+    });
+  };
 
-  /*
-  result_search
-    ? await add_tag(result_search).catch((e) => {
-        console.error("add_tag error");
-        res.status(500).json({
-          error: "add_tag error",
-        });
-        throw e;
-      })
-    : "";
-  */
+  res.json("success");
 });
 
 router.delete("/", async (req: express.Request, res: express.Response) => {
@@ -83,7 +69,7 @@ router.delete("/", async (req: express.Request, res: express.Response) => {
     : undefined; //errorになるかも
   const videoId = req.query.videoId as string | undefined;
 
-  if (typeof tagName == undefined && typeof videoId) {
+  if (typeof tagName == undefined && typeof videoId == undefined) {
     res.status(500).json("error");
   }
 
