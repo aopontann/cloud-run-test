a4942daa8acea5b17b75fd84dfcbf0e371465365
aopontann <aopontan0416@gmail.com>
2021-09-11 16:46:44 +0900

    routesからcontrollerに変更

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 require('events').EventEmitter.defaultMaxListeners = 15;
-// ライブラリ読み込み
+
 import express from "express"
-import youtube_router from "./routes/youtube"
-import youtube_auth from "./routes/youtube-oauth"
-import video_router from "./routes/videos"
-import vtuber_router from "./routes/vtuber"
-import tags_router from "./routes/tags"
-import twitter_router from "./routes/twitter"
+import youtube_controller from "./controllers/youtube"
+import youtube_auth from "./controllers/youtube-oauth"
+import video_controller from "./controllers/videos"
+import vtuber_controller from "./controllers/vtuber"
+import tags_controller from "./controllers/tags"
+import twitter_controller from "./controllers/twitter"
 import cros from "cors"
-import { authCheck } from "./authCheck";
+import { authCheck } from "./lib/authCheck";
 
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
-  console.log()
 }
 
 const app = express();
@@ -26,13 +25,14 @@ app.use(express.json({ limit: '10mb' }));
 
 const port = process.env.PORT || 8080; // port番号を指定
 
-app.use('/youtube', youtube_router);
+// ルーティング
+app.use('/youtube', youtube_controller);
 app.use('/youtube-auth', authCheck);
 app.use('/youtube-auth', youtube_auth);
-app.use('/videos', video_router);
-app.use('/vtuber', vtuber_router);
-app.use('/tags', tags_router);
-app.use('/twitter', twitter_router);
+app.use('/videos', video_controller);
+app.use('/vtuber', vtuber_controller);
+app.use('/tags', tags_controller);
+app.use('/twitter', twitter_controller);
 
 
 //サーバ起動
