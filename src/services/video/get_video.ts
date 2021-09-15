510c0ee3c42f93562f3301a21a51b760e160fa21
aopontann <aopontan0416@gmail.com>
2021-09-15 21:50:59 +0900

    検索結果数を表示する機能を追加 #44

--- src/services/video/get_video.ts
+++ src/services/video/get_video.ts
@@ -2,10 +2,10 @@ import { Thumbnails, Statistics, Videos } from "@prisma/client";
 import prisma from "../../lib/client";
 
 interface GetVideo {
-  videoId?: string[];
+  id?: string[];
   songConfirm?: boolean;
-  startAtAfter?: string;
-  startAtBefore?: string;
+  startTimeAtAfter?: string;
+  startTimeAtBefore?: string;
   maxResults?: number;
   page?: number;
   order?: string;
@@ -17,19 +17,34 @@ interface Include {
   statistic: Statistics | null;
 }
 
-export default async function (query: GetVideo): Promise<(Videos & Include)[]> {
-  const all_videoId = query?.videoId || null;
+export default async function (query: GetVideo) {
+  const all_videoId = query?.id || null;
   const songConfirm =
     typeof query?.songConfirm == "boolean" ? query.songConfirm : null;
-  const startAtAfter = query?.startAtAfter || null;
-  const startAtBefore = query?.startAtBefore || null;
+  const startAtAfter = query?.startTimeAtAfter || null;
+  const startAtBefore = query?.startTimeAtBefore || null;
   const order = query?.order || "viewCount";
-  const maxResults = query?.maxResults || 9999;
+  /* result_videos_totalCountの下に書いた
+    const maxResults = query?.maxResults || 9999;
+  */
   const page = query?.page && query.page > 0 ? query.page : 1;
   const tags = query?.tags || null;
   const NG_tags = ["test"];
 
-  const getVideo = await prisma.videos.findMany({
+  const result_videos_totalCount = await prisma.videos.count({
+    where: {
+      AND: [
+        { id: all_videoId ? { in: all_videoId } : undefined },
+        { songConfirm: songConfirm != null ? songConfirm : undefined },
+        { startTime: startAtAfter ? { gte: startAtAfter } : undefined },
+        { startTime: startAtBefore ? { lte: startAtBefore } : undefined },
+        { tags: tags ? { some: { name: { in: tags } } } : undefined },
+      ],
+    },
+  });
+  const maxResults = query?.maxResults || result_videos_totalCount;
+
+  const result_videos = await prisma.videos.findMany({
     where: {
       AND: [
         { id: all_videoId ? { in: all_videoId } : undefined },
@@ -48,7 +63,7 @@ export default async function (query: GetVideo): Promise<(Videos & Include)[]> {
             },
           },
     skip: maxResults * (page - 1),
-    take: order == "random" ? 9999 : maxResults,
+    take: order == "random" ? undefined : maxResults,
     include: {
       thumbnail: true,
       statistic: true,
@@ -64,11 +79,20 @@ export default async function (query: GetVideo): Promise<(Videos & Include)[]> {
   });
 
   if(order == "random"){
-    for (let i = getVideo.length - 1; i > 0; i--) {
+    for (let i = result_videos.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
-      [getVideo[i], getVideo[j]] = [getVideo[j], getVideo[i]];
+      [result_videos[i], result_videos[j]] = [result_videos[j], result_videos[i]];
     }
   }
 
-  return getVideo.slice(0, maxResults);
+  const pageInfo = {
+    totalResults: result_videos_totalCount,
+    resultsPerPage: result_videos.slice(0, maxResults).length,
+    nextPage: page*maxResults < result_videos_totalCount ? page+1 : undefined,
+    prevPage: page > 1 ? page-1 :undefined 
+  }
+  return {
+    pageInfo,
+    items: result_videos.slice(0, maxResults)
+  }
 }
