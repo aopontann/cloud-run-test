850ce4e1fddab5467e6cbc896c28170dcbbfe5cc
aopontann <aopontan0416@gmail.com>
2021-09-18 15:54:36 +0900

    add: type,birthday vtuberの種類(活動中,卒業など)や誕生日を指定できるようにした

--- src/services/vtuber/get_vtuber.ts
+++ src/services/vtuber/get_vtuber.ts
@@ -5,10 +5,14 @@ export default async function (query?: {
   affiliation?: string[];
   name?: string[];
   id?: string[];
+  type?: string[];
+  birthday?: string[];
 }): Promise<Vtuber[]> {
   const affi = query?.affiliation || null;
   const names = query?.name || null;
   const id = query?.id || null;
+  const type = query?.type || null;
+  const birthday = query?.birthday || null;
  
   const getVtuber = await prisma.vtuber
     .findMany({
@@ -17,6 +21,8 @@ export default async function (query?: {
           { affiliation: affi ? { in: affi } : undefined },
           { name: names ? { in: names } : undefined },
           { id: id ? { in: id } : undefined },
+          { type: type ? { in: type } : undefined },
+          { birthday: birthday ? { in: birthday } : undefined },
         ],
       },
       orderBy: {readname: "asc"}
