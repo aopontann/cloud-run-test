ddb0b01e0a15f79d4fbc20e0470134ead079a750
aopontann <aopontan0416@gmail.com>
2021-09-18 15:43:39 +0900

    create: validate_vtuber #44

--- src/controllers/vtuber.ts
+++ src/controllers/vtuber.ts
@@ -2,61 +2,86 @@ import express from "express";
 import get_vtuber from "../services/vtuber/get_vtuber";
 import add_vtuber from "../services/vtuber/add_vtuber";
 import update_vtuber from "../services/vtuber/update_vtuber";
+import {
+  validateQuery_vtuber,
+  validateBody_vtuber,
+} from "../validate/validate_vtuber";
 
 const router = express.Router();
 
-// http://localhost:8080/DB/vtuber?
-// affiliation= (にじさんじ, ...) & name= (える, ...)
 // クエリ指定しないと全てのデータが返ってくるよ
-router.get("/", async function (req: express.Request, res: express.Response): Promise<void> {
-  console.log("query", req.query);
-  const affi = (req.query?.affiliation as string) || null;
-  const names = (req.query?.names as string) || null;
-  const id = (req.query?.id as string) || null;
+router.get(
+  "/",
+  validateQuery_vtuber(),
+  async function (req: express.Request, res: express.Response): Promise<void> {
+    console.log("get vtuber query", req.query);
+    const affi = req.query.affiliation
+      ? String(req.query.affiliation).split(",")
+      : undefined;
+    const name = req.query.name ? String(req.query.name).split(",") : undefined;
+    const id = req.query.id ? String(req.query.id).split(",") : undefined;
+    const type = req.query.type ? String(req.query.type).split(",") : undefined;
+    const birthday = req.query.birthday
+      ? String(req.query.birthday).split(",")
+      : undefined;
 
-  const result = await get_vtuber({
-    affiliation: affi?.split(","),
-    name: names?.split(","),
-    id: id?.split(","),
-  }).catch((e) => {
-    console.log("get_vtuber error!");
-    res.status(500).json({
-      error: "get_vtuber error",
+    const result = await get_vtuber({
+      affiliation: affi,
+      name,
+      id,
+      type,
+      birthday,
+    }).catch((e) => {
+      console.log("get_vtuber error!");
+      res.status(500).json({
+        error: "get_vtuber error",
+      });
+      throw e;
     });
-    throw e;
-  });
-  //console.log(result);
-  res.status(200).json(result);
-});
+    //console.log(result);
+    res.status(200).json(result);
+  }
+);
 
-router.post("/", async function (req: express.Request, res: express.Response): Promise<void> {
-  await add_vtuber([...req.body]).catch((e) => {
-    console.log("add_vtuber error!", e);
-    res.status(500).json({
-      error: "add_vtuber error",
+router.post(
+  "/",
+  validateBody_vtuber(["id", "name", "readname", "affiliation", "type"]),
+  async function (req: express.Request, res: express.Response): Promise<void> {
+    
+    await add_vtuber({...req.body}).catch((e) => {
+      console.log("add_vtuber error!", e);
+      res.status(500).json({
+        error: "add_vtuber error",
+      });
+      throw e;
     });
-    throw e;
-  });
-  res.status(201).json("success");
-});
+    
+    res.status(201).json("success");
+  }
+);
 
-router.put("/", async function (req: express.Request, res: express.Response): Promise<void> {
-  await update_vtuber({
-    id: req.body.id,
-    name: req.body.name || null,
-    readname: req.body.readname || null,
-    affiliation: req.body.affiliation || null,
-    birthday: req.body.birthday || null,
-    image: req.body.image || null,
-  }).catch((e) => {
-    console.log("update_vtuber error!", e);
-    res.status(500).json({
-      error: "update_vtuber error",
+router.put(
+  "/",
+  validateBody_vtuber(["id"]),
+  async function (req: express.Request, res: express.Response): Promise<void> {
+    await update_vtuber({
+      id: req.body.id,
+      name: req.body.name || null,
+      readname: req.body.readname || null,
+      affiliation: req.body.affiliation || null,
+      birthday: req.body.birthday || null,
+      image: req.body.image || null,
+    }).catch((e) => {
+      console.log("update_vtuber error!", e);
+      res.status(500).json({
+        error: "update_vtuber error",
+      });
+      throw e;
     });
-    throw e;
-  });
-  res.status(201).json("success!");
-});
+
+    res.status(201).json("success!");
+  }
+);
 
 //routerをモジュールとして扱う準備
 export = router;
---
+++ src/validate/validate_vtuber.ts
@@ -0,0 +1,116 @@
+import express from "express";
+
+const target_validate = [
+  "affiliation",
+  "type",
+  "id",
+  "name",
+  "birthday",
+  "readname",
+  "image",
+];
+const all_affiliation = [
+  "にじさんじ",
+  "NIJISANJI KR",
+  "NIJISANJI ID",
+  "NIJISANJI IN",
+  "NIJISANJI EN",
+];
+const all_type = ["活動中", "卒業", "ユニット", "公式"];
+
+export const validateBody_vtuber =
+  (requireKeys: string[]) =>
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    // affiliation バリデーションチェック
+    const body = req.body;
+    if (body.affiliation) {
+      for (const affi of String(body.affiliation).split(",")) {
+        if (!all_affiliation.includes(affi)) {
+          res.status(400).json({ errors: "NG affiliation" });
+        }
+      }
+    }
+
+    // type バリデーションチェック
+    if (body.type) {
+      for (const type of String(body.type).split(",")) {
+        if (!all_type.includes(type)) {
+          res.status(400).json({ errors: "NG type" });
+        }
+      }
+    }
+
+    if (body.birthday) {
+      if (!String(body.birthday).match(/[0-9]{4}/)) {
+        res.status(400).json({ errors: "NG birthday" });
+      }
+    }
+
+    for (const [key, val] of Object.entries(body)) {
+      // 「""」文字排除
+      if (val === "") {
+        res.status(400).json({ errors: `NG ${key}` });
+      }
+      // target_validate以外のkey排除
+      if (!target_validate.includes(key)) {
+        res.status(400).json({ errors: `Cannot be specified ${key}` });
+      }
+    }
+    // 必須パラメータが全て含まれているか
+    for (const requireKey of requireKeys) {
+      if (!Object.keys(body).includes(requireKey)) {
+        res.status(400).json({ errors: `not found require key ${requireKey}` });
+      }
+    }
+
+    next();
+  };
+
+export const validateQuery_vtuber =
+  (requireKeys?: string[]) =>
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const query = req.query || {};
+    console.log("query", query);
+    // affiliation バリデーションチェック
+    if (query.affiliation) {
+      for (const affi of String(query.affiliation).split(",")) {
+        if (!all_affiliation.includes(affi)) {
+          res.status(400).json({ errors: "NG affiliation" });
+        }
+      }
+    }
+
+    // type バリデーションチェック
+    if (query.type) {
+      for (const type of String(query.type).split(",")) {
+        if (!all_type.includes(type)) {
+          res.status(400).json({ errors: "NG type" });
+        }
+      }
+    }
+
+    if (query.birthday) {
+      if (!String(query.birthday).match(/[0-9]{4}/)) {
+        res.status(400).json({ errors: "NG birthday" });
+      }
+    }
+
+    for (const [key, val] of Object.entries(query)) {
+      // 「""」文字排除
+      if (val === "") {
+        res.status(400).json({ errors: `NG ${key}` });
+      }
+      // target_validate以外のkey排除
+      if (!target_validate.includes(key)) {
+        res.status(400).json({ errors: `Cannot be specified ${key}` });
+      }
+    }
+    // 必須パラメータが全て含まれているか
+    for (const requireKey of requireKeys || []) {
+      if (!Object.keys(query).includes(requireKey)) {
+        res.status(400).json({ errors: `not found require key ${requireKey}` });
+      }
+    }
+
+    next();
+  };
