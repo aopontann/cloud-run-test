1c4e5158a393fa096916f36ba6d32bdc27233ca0
aopontann <aopontan0416@gmail.com>
2021-09-24 13:19:47 +0900

    🚚 path変更 #44

--- src/controllers/vtuber.ts
+++ src/controllers/vtuber.ts
@@ -2,17 +2,16 @@ import express from "express";
 import get_vtuber from "../services/vtuber/get_vtuber";
 import add_vtuber from "../services/vtuber/add_vtuber";
 import update_vtuber from "../services/vtuber/update_vtuber";
-import {
-  validateQuery_vtuber,
-  validateBody_vtuber,
-} from "../validate/validate_vtuber";
+import { validateQuery_vtuber } from "../validate/vtuber/validate_get_vtuber";
+import { validateBody_POST_vtuber } from "../validate/vtuber/validate_post_vtuber";
+import { validateBody_PUT_vtuber } from "../validate/vtuber/validate_put_vtuber";
 
 const router = express.Router();
 
 // クエリ指定しないと全てのデータが返ってくるよ
 router.get(
   "/",
-  validateQuery_vtuber(),
+  validateQuery_vtuber,
   async function (req: express.Request, res: express.Response): Promise<void> {
     console.log("get vtuber query", req.query);
     const affi = req.query.affiliation
@@ -45,33 +44,25 @@ router.get(
 
 router.post(
   "/",
-  validateBody_vtuber(["id", "name", "readname", "affiliation", "type"]),
+  validateBody_POST_vtuber,
   async function (req: express.Request, res: express.Response): Promise<void> {
-    
-    await add_vtuber({...req.body}).catch((e) => {
+    await add_vtuber({ ...req.body }).catch((e) => {
       console.log("add_vtuber error!", e);
       res.status(500).json({
         error: "add_vtuber error",
       });
       throw e;
     });
-    
+
     res.status(201).json("success");
   }
 );
 
 router.put(
   "/",
-  validateBody_vtuber(["id"]),
+  validateBody_PUT_vtuber,
   async function (req: express.Request, res: express.Response): Promise<void> {
-    await update_vtuber({
-      id: req.body.id,
-      name: req.body.name || null,
-      readname: req.body.readname || null,
-      affiliation: req.body.affiliation || null,
-      birthday: req.body.birthday || null,
-      image: req.body.image || null,
-    }).catch((e) => {
+    await update_vtuber({ ...req.body }).catch((e) => {
       console.log("update_vtuber error!", e);
       res.status(500).json({
         error: "update_vtuber error",
