c85bbbe6ad9934616b908b4adbc7d65de785170f
aopontann <aopontan0416@gmail.com>
2021-09-27 23:46:59 +0900

    supertestのためファイル分割 #45

--- src/app.ts
+++ src/app.ts
@@ -10,20 +10,13 @@ import twitter_controller from "./controllers/twitter"
 import cros from "cors"
 import { authCheck } from "./lib/authCheck";
 
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
-}
-
 const app = express();
 
 // ローカルのクライアントからフェッチできるように
 app.use(cros());
 
-//app.use(express.urlencoded({ extended: true }));
 // ↓これがないと(POST)bodyが受け取れなくなる
 app.use(express.json({ limit: '10mb' }));
-
-const port = process.env.PORT || 8080; // port番号を指定
 
 // ルーティング
 app.use('/youtube', youtube_controller);
@@ -34,7 +27,4 @@ app.use('/vtuber', vtuber_controller);
 app.use('/tags', tags_controller);
 app.use('/twitter', twitter_controller);
 
-
-//サーバ起動
-app.listen(port);
-console.log('niji-song-server port ' + port);
+export default app;
